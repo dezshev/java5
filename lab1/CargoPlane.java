8127931f208ef063ecf7cfc79b4205df640649f2
dezshev <dezshev@gmail.com>
2024-12-25 20:53:09 +0300

    Добавлен графический интерфейс AirplaneManagerGUI

--- lab1/CargoPlane.java
+++
@@ -1,7 +0,0 @@
-package com.airport;
-
-public class CargoPlane extends Airplane {
-    public CargoPlane(String name, String flightName, int passengerCount, double fuelAmount) {
-        super(name, flightName, passengerCount, fuelAmount);
-    }
-}
